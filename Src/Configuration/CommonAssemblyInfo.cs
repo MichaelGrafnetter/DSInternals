0a733c6222e39b9d5f92dc76be21341c6ff8dd1d
MichaelGrafnetter <michael.grafnetter@outlook.com>
2016-01-14 12:38:27 +0100

    Prepare for the 2.10 release

--- Src/Configuration/CommonAssemblyInfo.cs
+++ Src/Configuration/CommonAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyProduct("DSInternals PowerShell Module")]
-[assembly: AssemblyCopyright("Copyright © 2015 Michael Grafnetter. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016 Michael Grafnetter. All rights reserved.")]
 
-[assembly: AssemblyVersion("2.9")]
-[assembly: AssemblyFileVersion("2.9")]
+[assembly: AssemblyVersion("2.10")]
+[assembly: AssemblyFileVersion("2.10")]
--- Src/Configuration/CommonAssemblyInfo.h
+++ Src/Configuration/CommonAssemblyInfo.h
@@ -1,7 +1,7 @@
 using namespace System::Reflection;
 
 [assembly:AssemblyProductAttribute(L"DSInternals PowerShell Module")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright © 2015 Michael Grafnetter. All rights reserved.")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright © 2015-2016 Michael Grafnetter. All rights reserved.")];
 
 // Note: Do not forget to change the version in app.rc files.
-[assembly:AssemblyVersionAttribute("2.9")];
+[assembly:AssemblyVersionAttribute("2.10")];
