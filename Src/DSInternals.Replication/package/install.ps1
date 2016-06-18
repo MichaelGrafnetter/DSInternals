6640562faf87547e0f22f957f33fb52255659bfe
MichaelGrafnetter <michael.grafnetter@outlook.com>
2016-06-18 19:47:50 +0200

    Updated NuGet package dependencies

--- Src/DSInternals.Replication/package/install.ps1
+++ Src/DSInternals.Replication/package/install.ps1
@@ -2,7 +2,7 @@
 
 # Remove unnecessary references from the project
 $project.Object.References |
-	where { $_.Name -like 'NDceRpc.*' -or $_.Name -in 'protobuf-net','DSInternals.Common' } |
+	where { ($_.Name -like 'NDceRpc.*' -and $_.Name -ne 'NDceRpc.Microsoft') -or $_.Name -eq 'protobuf-net' } |
 	foreach { $_.Remove() }
 
 
