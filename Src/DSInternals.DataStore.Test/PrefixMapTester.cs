41a5f6a1c45cf2a2612128bfa7a88efb01cf7c47
MichaelGrafnetter <michael.grafnetter@outlook.com>
2016-08-29 17:50:45 +0200

    PrefixMap tests update

--- Src/DSInternals.DataStore.Test/PrefixMapTester.cs
+++ Src/DSInternals.DataStore.Test/PrefixMapTester.cs
@@ -7,8 +7,11 @@ namespace DSInternals.DataStore.Test
     [TestClass]
     public class PrefixMapTester
     {
-        // Sample prefix map after Exchange 2016 schema import
+        // Sample prefix map after Exchange 2016 schema import.
         private static byte[] ExchangeBinaryPrefixMap = "060000006200000023480b002a864886f7140104b658664f310c002a864886f7140104b658668331090c002a864886f7140105b6583e83dc600a002a864886f714010614014b6d0a002a864886f714010614024f750b002a864886f7140106140183".HexToBinary();
+
+        // Sample prefix map Christoffer Andersson. Contains duplicate prefixes, which is a little bit strange.
+        private static byte[] SamplePrefixMap = "0E000000D800000023480B002A864886F7140104B65866BE180B002A864886F7140105B6583E84670C002A864886F7140104B6586683E14A0C002A864886F7140104B65866816C3D0C002A864886F7140105B6583E81D62C0C002A864886F7140105B6583E83AE720A002A864886F7140106140152690A002A864886F71401061402905F0A002A864886F7140106180249160A002A864886F71401061801F16D0B002A864886F71401061D0201F15A0B002A864886F71401061D0202BB410B002A864886F7140106140183E92609002A864886F714010283".HexToBinary();
 
         [TestMethod]
         public void PrefixMap_Vector1()
@@ -28,6 +31,15 @@ namespace DSInternals.DataStore.Test
             // Test non-existing prefix
             bool contains2 = map.ContainsPrefix(1234);
             Assert.AreEqual(false, contains2);
+        }
+
+        [TestMethod]
+        public void PrefixMap_Vector2()
+        {
+            var map = new PrefixMap(SamplePrefixMap);
+
+            // Should contain 31 builtin refixes + 14 additional
+            Assert.AreEqual(45, map.Count);
         }
 
         [TestMethod]
