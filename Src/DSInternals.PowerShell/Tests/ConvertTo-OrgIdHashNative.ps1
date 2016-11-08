99cba912f27544e1ade89dc086b3fab97fc1d5af
MichaelGrafnetter <michael.grafnetter@outlook.com>
2016-11-08 22:47:28 +0100

    Added test generation code for #34

---
+++ Src/DSInternals.PowerShell/Tests/ConvertTo-OrgIdHashNative.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+Calculates the OrgId hash of a given password using Azure AD Connect's library.
+.DESCRIPTION
+This is only used to manually generate test data for C# Unit Tests.
+#>
+#Requires -Module DSInternals
+param(
+    [Parameter(Mandatory = $true)]
+    [SecureString] $Password
+)
+
+## Load the Microsoft.Online.PasswordSynchronization.Cryptography.dll assembly from Azure AD Connect
+# Location is typically 'C:\Program Files\Microsoft Azure AD Sync\', but it can be changed during installation.
+$adSyncPath = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\AD Sync').Location 
+$cryptoAssemblyPath = Join-Path $adSyncPath 'Bin\Microsoft.Online.PasswordSynchronization.Cryptography.dll'
+Add-Type -Path $cryptoAssemblyPath -ErrorAction Stop
+
+$ntHash = ConvertTo-NTHash -Password $Password
+return [Microsoft.Online.PasswordSynchronization.OrgIdHashGenerator]::Generate($ntHash.ToUpper())
