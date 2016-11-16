cad6cd1c9d2306832424689670efd0338d8fca40
MichaelGrafnetter <michael.grafnetter@outlook.com>
2016-11-16 15:37:57 +0100

    Renamed the properties of SamDomainPasswordInformation to mimic AD module behavior

--- Src/DSInternals.SAM/Interop/Structs/SamDomainPasswordInformation.cs
+++ Src/DSInternals.SAM/Interop/Structs/SamDomainPasswordInformation.cs
@@ -14,17 +14,17 @@ namespace DSInternals.SAM.Interop
         /// <summary>
         /// Specifies the minimum length, in characters, of a valid password.
         /// </summary>
-        public ushort MinimumPasswordLength;
+        public ushort MinPasswordLength;
         
         /// <summary>
         /// Indicates the number of previous passwords saved in the history list. A user cannot reuse a password in the history list.
         /// </summary>
-        public ushort PasswordHistorySize;
+        public ushort PasswordHistoryCount;
 
         /// <summary>
         /// Flags that describe the password properties
         /// </summary>
-        public SamDomainPasswordProperties PasswordProperties;
+        private SamDomainPasswordProperties PasswordProperties;
 
         private long maxPasswordAge;
         private long minPasswordAge;
@@ -32,7 +32,7 @@ namespace DSInternals.SAM.Interop
         /// <summary>
         /// Specifies the maximum length of time that a password can remain the same. Passwords older than this must be changed. Because SAM stores relative times as negative values and absolute times as positive numbers, the time is stored as a FILETIME structure with negative values.
         /// </summary>
-        public TimeSpan MaximumPasswordAge
+        public TimeSpan MaxPasswordAge
         {
             get
             {
@@ -47,15 +47,64 @@ namespace DSInternals.SAM.Interop
         /// <summary>
         /// Specifies the minimum length of time before a password can be changed. Because SAM stores relative times as negative values and absolute times as positive numbers, the time is stored as a FILETIME structure with negative values.
         /// </summary>
-        public TimeSpan MinimumPasswordAge
+        public TimeSpan MinPasswordAge
         {
             get
             {
-                return -new TimeSpan(minPasswordAge);
+                return -new TimeSpan(this.minPasswordAge);
             }
             set
             {
-                minPasswordAge = value.Ticks;
+                this.minPasswordAge = value.Ticks;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether passwords must meet complexity requirements.
+        /// </summary>
+        public bool ComplexityEnabled
+        {
+            get
+            {
+                return this.PasswordProperties.HasFlag(SamDomainPasswordProperties.PasswordComplexity);
+            }
+            set
+            {
+                if(value == true)
+                {
+                    // Add flag
+                    this.PasswordProperties |= SamDomainPasswordProperties.PasswordComplexity;
+                }
+                else
+                {
+                    // Remove flag
+                    this.PasswordProperties &= ~SamDomainPasswordProperties.PasswordComplexity;
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the "Store passwords using reversible encryption" option is enabled.
+        /// </summary>
+        public bool ReversibleEncryptionEnabled
+        {
+            get
+            {
+                return this.PasswordProperties.HasFlag(SamDomainPasswordProperties.ClearTextPassword);
+            }
+            set
+            {
+                if (value == true)
+                {
+                    // Add flag
+                    this.PasswordProperties |= SamDomainPasswordProperties.ClearTextPassword;
+                }
+                else
+                {
+                    // Remove flag
+                    this.PasswordProperties &= ~SamDomainPasswordProperties.ClearTextPassword;
+                }
             }
         }
     }
