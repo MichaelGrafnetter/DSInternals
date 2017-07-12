e5dfcd0c87e7b78cde56a02274adaf6cd1d8465c
Michael Grafnetter <michael.grafnetter@outlook.com>
2017-07-12 20:31:52 +0200

    Added strong name test script.

---
+++ Scripts/Test-StrongName.ps1
@@ -0,0 +1,20 @@
+﻿# TODO: Convert this into a Pester test
+Get-ChildItem ..\Build\bin\Release\DSInternals -Recurse -Filter *.dll | foreach {
+    try
+    {
+        $assemblyName = [System.Reflection.AssemblyName]::GetAssemblyName($PSItem.FullName)
+        $isSigned = $assemblyName.Flags.HasFlag([System.Reflection.AssemblyNameFlags]::PublicKey)
+        if($isSigned)
+        {
+            Write-Host ('Found assembly with strong name: {0}' -f $assemblyName.FullName)
+        }
+        else
+        {
+            throw "The assembly $PSItem does not have a strong name."
+        }
+    }
+    catch [System.BadImageFormatException]
+    {
+        # The DLL file is not a .NET assembly. We can ignore this error, because it is probably the Visual C++ Runtime. 
+    }
+}
