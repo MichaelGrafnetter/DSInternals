44a1eb009f7d6b74a8046fb1fb32eb39e67a991f
Michael Grafnetter <michael.grafnetter@outlook.com>
2019-01-01 11:12:45 +0100

    Smoke tests, test cleanup

--- Src/DSInternals.PowerShell/Tests/ConvertFrom-UnicodePassword.Tests.ps1
+++ Src/DSInternals.PowerShell/Tests/ConvertFrom-UnicodePassword.Tests.ps1
@@ -1,18 +1,18 @@
 Describe "ConvertFrom-UnicodePassword" {
 	Context "When correct input is provided" {
-        $input = 'UABhACQAJAB3ADAAcgBkAEEAZABtAGkAbgBpAHMAdAByAGEAdABvAHIAUABhAHMAcwB3AG8AcgBkAA=='
+        $testInput = 'UABhACQAJAB3ADAAcgBkAEEAZABtAGkAbgBpAHMAdAByAGEAdABvAHIAUABhAHMAcwB3AG8AcgBkAA=='
 		$expected = 'Pa$$w0rd'
-        $actual = ConvertFrom-UnicodePassword $input
+        $actual = ConvertFrom-UnicodePassword $testInput
 
         It "should return the correct password" {
             $actual | Should BeExactly $expected
         }
     }
 	Context "When the input is malformed" {
-        $input = 'abc'
+        $testInput = 'abc'
 
         It "should throw an exception" {
-            { ConvertFrom-UnicodePassword $input } | Should Throw
+            { ConvertFrom-UnicodePassword $testInput } | Should Throw
         }
     }
 }
--- Src/DSInternals.PowerShell/Tests/ConvertTo-LMHash.Tests.ps1
+++ Src/DSInternals.PowerShell/Tests/ConvertTo-LMHash.Tests.ps1
@@ -1,46 +1,46 @@
 Describe "ConvertTo-LMHash" {
 	Context "When the input is a unicode string" {
-        $input = ConvertTo-SecureString 'ûluùouËk˝ k˘Ú' -AsPlainText -Force 
+        $testInput = ConvertTo-SecureString 'ûluùouËk˝ k˘Ú' -AsPlainText -Force 
         $expected = "AAD3B435B51404EEAAD3B435B51404EE"
-        $actual = ConvertTo-LMHash $input
+        $actual = ConvertTo-LMHash $testInput
 			 
         It "should return the correct hash" {
             $actual | Should Be $expected
         }
     }
 	Context "When the input is an ASCII string" {
-        $input = ConvertTo-SecureString 'Pa$$w0rd' -AsPlainText -Force 
+        $testInput = ConvertTo-SecureString 'Pa$$w0rd' -AsPlainText -Force 
         $expected = "727E3576618FA1754A3B108F3FA6CB6D"
-        $actual = ConvertTo-LMHash $input
+        $actual = ConvertTo-LMHash $testInput
 			 
         It "should return the correct hash" {
             $actual | Should Be $expected
         }
     }
 	Context "When the input comes from the pipeline" {
-        $input1 = ConvertTo-SecureString 'Pa$$w0rd' -AsPlainText -Force
-		$input2 = ConvertTo-SecureString 'test' -AsPlainText -Force
+        $testInput1 = ConvertTo-SecureString 'Pa$$w0rd' -AsPlainText -Force
+		$testInput2 = ConvertTo-SecureString 'test' -AsPlainText -Force
         $expected = "727E3576618FA1754A3B108F3FA6CB6D","01FC5A6BE7BC6929AAD3B435B51404EE" 
-        $actual = $input1,$input2 | ConvertTo-LMHash
+        $actual = $testInput1,$testInput2 | ConvertTo-LMHash
 			 
         It "should return multiple hashes" {
             $actual | Should Be $expected
         }
     }
 	Context "When the input is an empty string" {
-        $input = New-Object SecureString
+        $testInput = New-Object SecureString
         $expected = "AAD3B435B51404EEAAD3B435B51404EE"
-        $actual = ConvertTo-LMHash $input
+        $actual = ConvertTo-LMHash $testInput
 			 
         It "should return the correct hash" {
             $actual | Should Be $expected
         }
     }
 	Context "When the input is a long string" {
-        $input = ConvertTo-SecureString 'EHB3xUAY2NZIp9wI7khNWGWyOiuhyK' -AsPlainText -Force
+        $testInput = ConvertTo-SecureString 'EHB3xUAY2NZIp9wI7khNWGWyOiuhyK' -AsPlainText -Force
 
         It "should throw an exception" {
-            { ConvertTo-LMHash $input } | Should Throw
+            { ConvertTo-LMHash $testInput } | Should Throw
         }
     }
 }
