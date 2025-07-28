298270f179be9e623f153ea5fde7b583862bdacb
Michael Grafnetter <michael.grafnetter@outlook.com>
2025-07-28 11:11:48 +0200

    Improved DSNAME parser.

--- Src/DSInternals.Replication.Model/DSName.cs
+++ Src/DSInternals.Replication.Model/DSName.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Buffers.Binary;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Messaging;
 using System.Security.Principal;
 using System.Text;
 
@@ -17,10 +15,19 @@ namespace DSInternals.Replication
         private const int SidMaxLength = 28;
         private const int DSNameHeaderSize = 2 * sizeof(uint) + GuidLength + SidMaxLength + sizeof(uint);
 
+        /// <summary>
+        /// The value of the object's objectSid attribute, its security identifier.
+        /// </summary>
         public SecurityIdentifier? ObjectSid { get; private set; }
 
+        /// <summary>
+        /// The value of the object's objectGUID attribute
+        /// </summary>
         public Guid? ObjectGuid { get; private set; }
 
+        /// <summary>
+        /// The object's distinguishedName attribute.
+        /// </summary>
         public string? DistinguishedName { get; private set; }
 
         public DSName(SecurityIdentifier? objectSid = null, Guid? objectGuid = null, string? distinguishedName = null)
@@ -47,8 +54,10 @@ namespace DSInternals.Replication
             int currentPosition = 0;
 
             // The length, in bytes, of the entire data structure.
-            uint structLen = BinaryPrimitives.ReadUInt32LittleEndian(buffer.Slice(currentPosition, sizeof(uint)));
-            currentPosition += sizeof(uint);
+            // Lengths in this structure are always in little-endian format.
+            // Although the lengths are defined as unsigned integers, we treat them as signed, because .NET API expects lengths to be signed integers.
+            int structLen = BinaryPrimitives.ReadInt32LittleEndian(buffer.Slice(currentPosition, sizeof(int)));
+            currentPosition += sizeof(int);
 
             if (structLen != buffer.Length)
             {
@@ -56,37 +65,46 @@ namespace DSInternals.Replication
             }
 
             // The number of bytes in the Sid field used to represent the object's objectSid attribute value.
-            uint sidLen = BinaryPrimitives.ReadUInt32LittleEndian(buffer.Slice(currentPosition, sizeof(uint)));
-            currentPosition += sizeof(uint);
+            int sidLen = BinaryPrimitives.ReadInt32LittleEndian(buffer.Slice(currentPosition, sizeof(int)));
+            currentPosition += sizeof(int);
 
-            if (sidLen > SidMaxLength)
+            if (sidLen > SidMaxLength || sidLen < 0)
             {
                 throw new ArgumentException($"Invalid SID length. Expected at most {SidMaxLength} bytes, but got {sidLen} bytes.", nameof(buffer));
             }
 
             // The value of the object's objectGUID attribute.
-            // TODO: For .NET Core, omit Span.ToArray()
-            Guid parsedGuid = new Guid(buffer.Slice(currentPosition, GuidLength).ToArray());
+#if NET
+            // This more efficient constructor is available in Core 2.1 and later.
+            var binaryGuid = buffer.Slice(currentPosition, GuidLength);
+#else
+            var binaryGuid = buffer.Slice(currentPosition, GuidLength).ToArray();
+#endif
+            Guid parsedGuid = new Guid(binaryGuid);
             currentPosition += GuidLength;
 
             if (parsedGuid != Guid.Empty)
             {
+                // If the values for all fields in the GUID structure are zero,
+                // this indicates that the DSNAME does not identify the objectGUID value of the directory object.
                 result.ObjectGuid = parsedGuid;
             }
 
             // The value of the object's objectSid attribute.
             if (sidLen > 0)
             {
-                result.ObjectSid = ParseSecurityIdentifier(buffer.Slice(currentPosition, (int)sidLen));
+                // Span.ToArray() does not seem efficient, but SecurityIdentifier(IntPtr) internally does the same and has a risk of buffer overflow.
+                byte[] binarySid = buffer.Slice(currentPosition, sidLen).ToArray();
+                result.ObjectSid = new SecurityIdentifier(binarySid, offset: 0);
             }
 
             // The SID has a constant length, even if not all bytes are used.
             currentPosition += SidMaxLength;
 
             // The number of characters in the StringName field, not including the terminating null character.
-            uint nameLen = BinaryPrimitives.ReadUInt32LittleEndian(buffer.Slice(currentPosition, sizeof(uint)));
-            currentPosition += sizeof(uint);
-            uint expectedNameLen = (structLen - DSNameHeaderSize) / sizeof(char) - 1;
+            int nameLen = BinaryPrimitives.ReadInt32LittleEndian(buffer.Slice(currentPosition, sizeof(int)));
+            currentPosition += sizeof(int);
+            int expectedNameLen = (structLen - DSNameHeaderSize) / sizeof(char) - 1;
 
             if (nameLen != expectedNameLen)
             {
@@ -96,7 +114,7 @@ namespace DSInternals.Replication
             if (nameLen > 0)
             {
                 // A null-terminated Unicode value of the object's distinguishedName attribute.
-                result.DistinguishedName = ParseUnicodeString(buffer.Slice(currentPosition, (int)(nameLen * sizeof(char))));
+                result.DistinguishedName = ParseUnicodeString(buffer.Slice(currentPosition, nameLen * sizeof(char)));
             }
 
             return result;
@@ -107,14 +125,6 @@ namespace DSInternals.Replication
             fixed (byte* pBuffer = buffer)
             {
                 return Encoding.Unicode.GetString(pBuffer, buffer.Length);
-            }
-        }
-
-        unsafe private static SecurityIdentifier ParseSecurityIdentifier(ReadOnlySpan<byte> buffer)
-        {
-            fixed (byte* pBuffer = buffer)
-            {
-                return new SecurityIdentifier((IntPtr)pBuffer);
             }
         }
     }
